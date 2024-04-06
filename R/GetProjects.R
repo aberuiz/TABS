4adcc723fde315fe022b1efa3bc7cb2216ae8a2a
Abraham Ruiz <abraham@Mac-mini-2.local>
2024-04-06 00:16:14 -0500

    init

---
+++ R/GetProjects.R
@@ -0,0 +1,60 @@
+GetProjects <- function(county = NULL, city = NULL, reg_begin = "", reg_end = "", owner = "", project = "", facility = "", address = ""){
+  # Registration date format must be in mm/dd/yy %D
+  reg_beg_formatted <- gsub("/","%2F",reg_begin)
+  reg_end_formatted <- gsub("/","%2F",reg_end)
+  # Decode city &/or county character value to integer for request
+  if (!is.null(county)){
+    countycodes <- subset(dat, dat$code > 1999)
+    county <- TABSdecoder(county, codebook_df = countycodes)
+  }
+
+  if (!is.null(city)){
+    citycodes <- subset(dat, dat$code < 2000)
+    city <- TABSdecoder(city, codebook_df = citycodes)
+  }
+
+  all_data <- list()
+  page <- 0
+
+  while(TRUE){
+    response <- httr2::request("https://www.tdlr.texas.gov/TABS/Search/SearchProjects") |>
+      httr2::req_headers(
+        `Accept` = "application/json",
+        `Connection` = "keep-alive"
+      ) |>
+      httr2::req_body_raw(
+        body = paste0("&start=", page * 100,
+                      "&length=100
+                      &search%5Bvalue%5D=true
+                      &search%5Bregex%5D=false",
+                      "&RegistrationDateBegin=",reg_beg_formatted,
+                      "&RegistrationDateEnd=",reg_end_formatted,
+                      "&OwnerName=",owner,
+                      "&ProjectName=",project,
+                      "&FacilityName=",facility,
+                      "&LocationAddress=",address,
+                      "&LocationCity=",city,
+                      "&LocationCounty=",county),
+        "application/x-www-form-urlencoded; charset=UTF-8"
+      ) |>
+      httr2::req_perform() |>
+      httr2::resp_body_json()
+    data <- dplyr::bind_rows(response$data)
+
+    if (nrow(data) == 0){
+      break
+    }
+    all_data <- dplyr::bind_rows(all_data, data)
+    page <- page + 1
+  }
+  if (length(all_data) == 0){
+    warning("No projects found for the registration period")
+  } else {
+    all_data[c("ProjectStatus","City","County","TypeOfWork")] <- lapply(
+      all_data[c("ProjectStatus","City","County","TypeOfWork")],
+      TABSdecoder
+    )
+    return(all_data)
+  }
+#  return(all_data)
+}
---
+++ R/TABSdecoder.R
@@ -0,0 +1,17 @@
+TABSdecoder <- function(codes,..., codebook_df=dat) {
+  # Ensure the codebook dataframe has two columns: numeric_value and character_value
+  if (!all(c("code", "value") %in% colnames(codebook_df))) {
+    stop("Codebook dataframe must have columns 'code' and 'value'")
+  }
+
+  if (is.numeric(codes)){
+    # Use match to find the corresponding character values based on numeric values
+    character_values <- codebook_df$value[match(codes, codebook_df$code)]
+
+    return(character_values)
+  } else {
+    numeric_values <- codebook_df$code[match(codes, codebook_df$value)]
+
+    return(numeric_values)
+  }
+}
