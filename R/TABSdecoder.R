0798d930c20f23ab3e43caf974857d2c56f2c5d6
Abraham Ruiz <abraham@workersdefense.org>
2025-09-17 10:36:23 -0500

    pkg structure & data

--- R/TABSdecoder.R
+++ R/TABSdecoder.R
@@ -1,8 +1,11 @@
 #' Recode values to and from TDLR
 #'
+#' @param codes variable sent to codebook for deciphering
+#' @param codebook_df codebook dataframe to check codes against, by default this uses the built in `codebook`
+#'
 #' @description
 #' Will submit appropriate codes to TDLR and return plain language values to user
-TABSdecoder <- function(codes,..., codebook_df=codebook) {
+TABSdecoder <- function(codes, codebook_df=codebook) {
   # Ensure the codebook dataframe has two columns: numeric_value and character_value
   if (!all(c("code", "value") %in% colnames(codebook_df))) {
     stop("Codebook dataframe must have columns 'code' and 'value'")
